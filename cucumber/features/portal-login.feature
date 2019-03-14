34288f31911f83fbf8da1c1c803cc34ef265e03c
irtaimal <ir.taimal10@uniandes.edu.co>
2019-03-13 21:09:23 -0500

    include screenshots

--- cucumber/features/portal-login.feature
+++ cucumber/features/portal-login.feature
@@ -3,8 +3,9 @@ Feature: Login scenario
   Scenario Outline:  Multiple authentication in accounts
     Given I write the url of portal <accountsUrl> in  the browser
     When I write my credentials, login : <userName>  and password: <userPassword> and click in login button
-    Then I should view the redirection page with title: <redirectPortalTitle>
+    Then I should view the redirection page with content: <resultContent>
 
     Examples:
-      | accountsUrl                                    | userName                        | userPassword     | redirectPortalTitle         |
-      | 'https://todoist.com/Users/showLogin?mini=1'   | 'captuayonovoafredy@gmail.com'  |'1234567890'      | 'Todoist'     |
+      | accountsUrl                                    | userName                        | userPassword        | resultContent               |
+      | 'https://todoist.com/Users/showLogin?mini=1'   | 'captuayonovoafredy@gmail.com'  |'1234567890'         | 'Todoist'                   |
+      | 'https://todoist.com/Users/showLogin?mini=1'   | 'captuayonovoafredy@gmail.com'  |'wrongPassword'      | 'Email o contraseña incorrectos' |
--- cucumber/features/steps/portal-login.steps.js
+++ cucumber/features/steps/portal-login.steps.js
@@ -1,6 +1,7 @@
 const {expect} = require('chai');
 const {Given, When, Then} = require('cucumber');
 const puppeteer = require('puppeteer');
+const pathScreenShots = './screenshots/';
 
 global.testContext = {};
 
@@ -8,6 +9,7 @@ Given('I write the url of portal {string} in  the browser',
     {timeout: 20 * 1000}, async (portalUrl) => {
         const width = 1280;
         const height = 1200;
+        global.testContext.screenshotPath = `${pathScreenShots}${new Date().getTime()}`;
         global.testContext.browser = await puppeteer.launch({
             headless: false,
             launch: {},
@@ -26,6 +28,7 @@ Given('I write the url of portal {string} in  the browser',
         await global.testContext.page.goto(portalUrl);
         await global.testContext.page.setViewport({width: width, height: height});
         await global.testContext.page.waitForSelector("body");
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-login-step0.png`});
     });
 
 When('I write my credentials, login : {string}  and password: {string} and click in login button',
@@ -34,11 +37,13 @@ When('I write my credentials, login : {string}  and password: {string} and click
         await global.testContext.page.type('.login_singup_form .input_email', userLogin);
         await global.testContext.page.type('.login_singup_form .input_password', userPassword);
         await global.testContext.page.click('.login_singup_form .submit_btn');
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-login-step1.png`});
     });
 
-Then('I should view the redirection page with title: {string}', {timeout: 20 * 1000}, async (redirectPortalTitle) => {
+Then('I should view the redirection page with content: {string}', {timeout: 20 * 1000}, async (redirectPortalContent) => {
     await expect(await global.testContext.page.waitForSelector("body"));
-    const title = await global.testContext.page.mainFrame().title();
-    await expect(title).to.eql(redirectPortalTitle);
+    const content = await global.testContext.page.content();
+    expect(content).to.have.string(redirectPortalContent);
+    await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-login-step2.png`});
     await global.testContext.browser.close();
 });
