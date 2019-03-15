f1b89d361bcff880976aeea1b3ef0ff79bec4b6d
irtaimal <ir.taimal10@uniandes.edu.co>
2019-03-14 19:14:18 -0500

    add tasks steps

--- cucumber/features/portal-add-task.feature
+++ cucumber/features/portal-add-task.feature
@@ -1,7 +1,19 @@
 Feature: Register scenario
 
   Scenario: Add task
-    # Given I am authenticated on the site 'https://todoist.com/Users/showLogin?mini=1' with my credentials,  login : 'captuayonovoafredy@gmail.com'  and password: '1234567890'
-    # When I click in add task button
-    # Then I should view the result page with content: 'Email is already found in our database'
+    Given I am authenticated on the site 'https://todoist.com/Users/showLogin?mini=1' with my credentials,  login : 'captuayonovoafredy@gmail.com'  and password: '1234567890'
+    When I click in add task button
+    When I write a description of a new task: 'Task-test-2'
+    When I submit task creation
+    Then I should view 'Task-test-2' task in list
+
+
+  Scenario: Attempt to Add task
+    Given I am authenticated on the site 'https://todoist.com/Users/showLogin?mini=1' with my credentials,  login : 'captuayonovoafredy@gmail.com'  and password: '1234567890'
+    When I click in add task button
+    When I write a description of a new task: 'Task-test-1'
+    When I cancel task creation
+    Then I should view 'Task-test-1' task in list
+
+
 
--- cucumber/features/steps/portal-add-task.steps.js
+++ cucumber/features/steps/portal-add-task.steps.js
@@ -37,10 +37,51 @@ Given('I am authenticated on the site {string} with my credentials,  login : {st
     });
 
 When('I click in add task button',
-    {timeout: 20 * 1000}, async () => {
+    {timeout: 30 * 1000}, async () => {
         await expect(await global.testContext.page.waitForSelector("#quick_add_task_holder"));
-        await global.testContext.page.click('.agenda_view .agenda_add_task');
+        await global.testContext.page.waitFor(() => !document.querySelector(".loading-screen--logo"));
+        await global.testContext.page.waitFor(() => !document.querySelector(".loading_screen"));
         await global.testContext.page.click('#quick_add_task_holder');
         await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-step1.png`});
+    });
+
+
+When('I write a description of a new task: {string}',
+    {timeout: 20 * 1000}, async (taskDescription) => {
+        await global.testContext.page.waitFor(() => !document.querySelector(".loading-screen--logo"));
+        await global.testContext.page.waitFor(() => !document.querySelector(".loading_screen"));
+        await expect(await global.testContext.page.waitForSelector(".sel_richtext_editor"));
+        await global.testContext.page.click('.sel_richtext_editor');
+        await global.testContext.page.type('.sel_richtext_editor', taskDescription);
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-step2.png`});
+    });
+
+
+When('I cancel task creation',
+    {timeout: 20 * 1000}, async () => {
+        await expect(await global.testContext.page.waitForSelector(".g_header .close"));
+        await global.testContext.page.click('.g_header .close');
+        await expect(await global.testContext.page.waitForSelector(".alert_holder .ist_button_red"));
+        await global.testContext.page.click('.alert_holder .ist_button_red');
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-step3.png`});
+    });
+
+When('I submit task creation',
+    {timeout: 20 * 1000}, async () => {
+        await expect(await global.testContext.page.waitForSelector(".td_submit_area .submit_btn"));
+        await global.testContext.page.click('.td_submit_area .submit_btn');
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-step4.png`});
+    });
+
+
+Then('I should view {string} task in list',
+    {timeout: 20 * 1000}, async (taskDescription) => {
+        await expect(await global.testContext.page.waitForSelector(".items .task_item"));
+        const taskItems = await global.testContext.page.$$('.items .task_item');
+        const content = await global.testContext.page.content();
+        expect(content).to.have.string(taskDescription);
+        await global.testContext.page.screenshot({path: `${global.testContext.screenshotPath}-step5.png`});
         await global.testContext.browser.close();
     });
+
+
